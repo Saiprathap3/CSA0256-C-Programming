fd5ed9eecc50f44633723f274dc5da2516d21c48
Saiprathap3 <128614840+Saiprathap3@users.noreply.github.com>
2023-03-23 19:42:34 +0530

    output

---
+++ 23-03-2023 - 192210657 - c program.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num, reversedNum = 0, remainder;
+    
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    
+    for (; num != 0; num /= 10) {
+        remainder = num % 10;
+        reversedNum = reversedNum * 10 + remainder;
+    }
+    
+    printf("Reversed number: %d\n", reversedNum);
+    
+    return 0;
+}
