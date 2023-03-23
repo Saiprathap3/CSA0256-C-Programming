d711661dbdd6508166ef9f6babd21d2dd3b917ed
Saiprathap3 <128614840+Saiprathap3@users.noreply.github.com>
2023-03-23 20:32:49 +0530

    output

---
+++ 23-03-2023 - 192210657 - c program.7.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char s[] = "man";
+    int i;
+    for (i = 0; s[i] != '\0'; i++) {
+        printf("%c%c%c%c\n", s[i], *(s+i), *(i+s), i[s]);
+    }
+    return 0;
+}
